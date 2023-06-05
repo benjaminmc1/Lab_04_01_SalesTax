960ac8e64bb944a40600984816209194341244bc
Benjamin <bmckee566@gmail.com>
2023-06-05 13:16:15 -0400

    Task 1 finish

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        double salesTax = 0.05;
+        int purchaseCost = 20;
+        double taxCost;
+        double totalCost;
+
+        taxCost = purchaseCost * salesTax;
+        totalCost = taxCost + purchaseCost;
+
+        System.out.println("The total cost is " + totalCost + " and the cost of tax is " + taxCost); // Output statements
     }
 }
